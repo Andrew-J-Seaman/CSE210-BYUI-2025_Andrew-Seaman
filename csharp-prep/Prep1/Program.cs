e2dfc4592f950b72468523fbc6c680f446a2bd34
Andrew-J-Seaman <sea19006@byui.edu>
2024-09-23 14:07:21 -0600

    Moved my Prep1 work from the sandbox folder to csharp-prep>Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
+//Default
         Console.WriteLine("Hello Prep1 World!");
+
+//W01
+
+    //Learning Activity - C# Prep 1
+        //OBJ: Take input and print as output.
+
+        // Input: {first name}
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        
+        // Input: {last name}
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        
+        // Output: {first_name} & {last_name}
+        Console.WriteLine($"First name: {first_name}");
+        Console.WriteLine($"Last name: {last_name}");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,7 +1,5 @@
 using System;
 
-//DEFAULT######################
-/**
 class Program
 {
     static void Main(string[] args)
@@ -9,29 +7,3 @@ class Program
         Console.WriteLine("Hello Sandbox World!");
     }
 }
-**/
-
-//STUDY########################
-
-//W01
-
-    //Learning Activity - C# Prep 1
-        //OBJ: Take input and print as output.
-
-class One
-{
-    static void Main(string[] args)
-    {   //Input: {first name}
-        Console.Write("What is your first name? ");
-        string first_name = Console.ReadLine();
-        //Input: {last name}
-        Console.Write("What is your last name? ");
-        string last_name = Console.ReadLine();
-        //Output: {first_name} & {last_name}
-        Console.WriteLine($"First name: {first_name}");
-        Console.WriteLine($"Last name: {last_name}");
-
-
-    }
-}
-
