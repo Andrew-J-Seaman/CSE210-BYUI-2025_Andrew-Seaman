fce4c616eee9f9e245e04ef82670e7dbc3c003b1
Andrew-J-Seaman <sea19006@byui.edu>
2024-09-23 13:46:07 -0600

    Practices in the sandbox with taking input and printing as output.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,5 +1,7 @@
 using System;
 
+//DEFAULT######################
+/**
 class Program
 {
     static void Main(string[] args)
@@ -7,3 +9,29 @@ class Program
         Console.WriteLine("Hello Sandbox World!");
     }
 }
+**/
+
+//STUDY########################
+
+//W01
+
+    //Learning Activity - C# Prep 1
+        //OBJ: Take input and print as output.
+
+class One
+{
+    static void Main(string[] args)
+    {   //Input: {first name}
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        //Input: {last name}
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        //Output: {first_name} & {last_name}
+        Console.WriteLine($"First name: {first_name}");
+        Console.WriteLine($"Last name: {last_name}");
+
+
+    }
+}
+
