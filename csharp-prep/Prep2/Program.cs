0e801b3934d4b8967bbc96edfda85d5c1c15d28b
Andrew-J-Seaman <sea19006@byui.edu>
2024-09-23 20:15:23 -0600

    Prep2: built grading system.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,29 +1,55 @@
 using System;
+using System.Formats.Asn1;
 using System.Net.Sockets;
 
 class Program
 {
     static void Main(string[] args)
     {
-//Default
         Console.WriteLine("Hello Prep2 World!");
-//OBJ: work with variables and conditional statements in C# 
+//OBJ: create grading system
+/**
+A >= 90
+B >= 80
+C >= 70
+D >= 60
+F < 60
+**/
 
-//TOPICS:
-    /** CONDITIONAL STATEMENTS:
-            1. if (else if/else)
-    **/
-    /** LOOP TYPES:
-            1. for
-            2. while
-            3. do
-            4. do while (??) 
-    **/
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
 
+        string grade = "";
 
+        try
+        {
+            int percent = int.Parse(userInput);
 
-
-
-
+            if (percent >= 90)
+            {
+                grade = "A";
+            }
+            else if (percent >= 80)
+            {
+                grade = "B";
+            }
+            else if (percent >= 70)
+            {
+                grade = "C";
+            }
+            else if (percent >= 60)
+            {
+                grade = "D";
+            }
+            else if (percent < 60)
+            {
+                grade = "F";
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("That's not a valid number. Please enter a numeric value.");
+        }
+        Console.WriteLine($"You're grade is {grade}.");
     }
 }
