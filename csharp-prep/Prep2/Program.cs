04059cf86339a79ab46563118aefd1a220e27ee8
Andrew-J-Seaman <sea19006@byui.edu>
2024-09-23 20:41:58 -0600

    Finished Prep2 to meet core requirements.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,55 +1,71 @@
 using System;
 using System.Formats.Asn1;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
-//OBJ: create grading system
-/**
-A >= 90
-B >= 80
-C >= 70
-D >= 60
-F < 60
-**/
+        //OBJ: create grading system
+        /** Criteria:
+        A >= 90
+        B >= 80
+        C >= 70
+        D >= 60
+        F < 60
+        **/
 
         Console.Write("What is your grade percentage? ");
         string userInput = Console.ReadLine();
 
-        string grade = "";
+        string letter = "";
+        int percent = 0;
+        bool passed = true;
 
         try
         {
-            int percent = int.Parse(userInput);
+            percent = int.Parse(userInput);
 
             if (percent >= 90)
             {
-                grade = "A";
+                letter = "A";
             }
             else if (percent >= 80)
             {
-                grade = "B";
+                letter = "B";
             }
             else if (percent >= 70)
             {
-                grade = "C";
+                letter = "C";
             }
             else if (percent >= 60)
             {
-                grade = "D";
+                letter = "D";
             }
             else if (percent < 60)
             {
-                grade = "F";
+                letter = "F";
             }
         }
         catch (FormatException)
         {
             Console.WriteLine("That's not a valid number. Please enter a numeric value.");
         }
-        Console.WriteLine($"You're grade is {grade}.");
+
+        if (percent < 70)
+        {
+            passed = false;
+        }
+
+        if (passed == true)
+        {
+            Console.WriteLine($"You passed with a {letter}.");
+        }
+        else
+        {
+            Console.WriteLine($"You failed with a {letter}.");
+        }
     }
 }
