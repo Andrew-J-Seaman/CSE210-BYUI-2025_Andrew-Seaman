0c9d9191edc0e627ba42a675a2feaf758885c4b5
Andrew-J-Seaman <sea19006@byui.edu>
2024-10-06 18:17:18 -0600

    Created two new files for two new classes.

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,12 @@
+public class Job
+{
+    // Attributes
+    string _name;
+    public List<Job> _jobs = new List<Job>();
+    
+    // Methods (Member functions)
+    public void Display()
+    {
+        
+    }
+}
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,14 @@
+public class Resume
+{
+    // Attributes
+    string _company;
+    string _jobTitle;
+    int _startYear;
+    int _endYear;
+
+    // Methods (Member functions)
+    public void Display()
+    {
+        
+    }
+}
