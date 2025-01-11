304103611cd261927e2c4052432d552cb153017f
Andrew-J-Seaman <sea19006@byui.edu>
2025-01-11 16:49:47 -0700

    Finalized everything I want to do on this prep assignment but reformating the final output to use square brackets to indicate variable output is in effect.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,81 +4,118 @@ using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 
 // OBJECTIVE: create grading system
-        /** Criteria:
-            A >= 90
-            B >= 80
-            C >= 70
-            D >= 60
-            F < 60
-        **/
+/** Criteria:
+    A >= 90
+    B >= 80
+    C >= 70
+    D >= 60
+    F < 60
+**/
 
 class Program
 {
     static void Main(string[] args)
     {
-    // Greeting
+        // Greeting
         Console.Clear();
         Console.WriteLine("> Hello student!");
 
-    // Initialize  variables
-        string  _letter = "";
-        int     _percent = 0;
-        string  _passOrFail;
-        string  _congratsOrSympathies;
-        string  _letterSymbol;
-        string  _properArticle;
-        bool    _validPercent = false;
-        
-    // Ask user for input
+
+
+        // Initialize  variables
+        string _letter = "";
+        int _percent = 0;
+        string _passOrFail;
+        string _congratsOrSympathies;
+        string _letterSymbol;
+        string _properArticle;
+        bool _validPercent = false;
+        // Set variables based on verified user input
+        string percentString = _percent.ToString();
+        int lastChar = percentString[percentString.Length - 1];
+
+
+        // Ask user for input
         while (!_validPercent)
         {
             Console.Write("> What is your grade percentage?\n   > ");
             _percent = int.Parse(Console.ReadLine());
 
-            if (_percent > 0 && _percent < 200) 
-                {_validPercent = true;}
+            if (_percent > 0 && _percent < 200)
+            {
+                _validPercent = true;
+            }
             else
-                {Console.WriteLine("> ERROR: Invalid grade percentage. Enter a value 0-200.\n");}
+            {
+                Console.WriteLine("> ERROR: Invalid grade percentage. Enter a value 0-200.\n");
+            }
         }
 
-    // Set variables based on verified user input
-        string percentString = _percent.ToString();
-        int lastChar = percentString[percentString.Length - 1];
-        
-    // _letter grade
+
+
+        // _letter grade
         if (_percent >= 90)
-            {_letter = "A";}
+        {
+            _letter = "A";
+        }
         else if (_percent >= 80)
-            {_letter = "B";}
+        {
+            _letter = "B";
+        }
         else if (_percent >= 70)
-            {_letter = "C";}
+        {
+            _letter = "C";
+        }
         else if (_percent >= 60)
-            {_letter = "D";}
+        {
+            _letter = "D";
+        }
         else if (_percent <= 59)
-            {_letter = "F";}
+        {
+            _letter = "F";
+        }
 
-    // Pass or fail
+        // Pass or fail
         if (_percent >= 60)
-            {_passOrFail = "PASSED";
-            _congratsOrSympathies = "Congrats! You";}
-        else 
-            {_passOrFail = "FAILED";
-            _congratsOrSympathies = "Our sympathies, you";}
+        {
+            _passOrFail = "PASSED";
+            _congratsOrSympathies = "Congrats! You";
+        }
+        else
+        {
+            _passOrFail = "FAILED";
+            _congratsOrSympathies = "Our sympathies, you";
+        }
 
-    // + or –
+        // + or –
         if (lastChar >= 7 && _letter != "A" && _letter != "F")
-            {_letterSymbol = "+";}
+        {
+            _letterSymbol = "+";
+        }
         else if (lastChar < 3 && _letter != "F")
-            {_letterSymbol = "-";}
+        {
+            _letterSymbol = "-";
+        }
         else
-            {_letterSymbol = "";}
+        {
+            _letterSymbol = "";
+        }
 
-    // Proper article
+        // Proper article
         if (_letter == "A" || _letter == "F")
-            {_properArticle = "an";}
+        {
+            _properArticle = "an";
+        }
         else
-            {_properArticle = "a";}
-    // Final output
-        Console.WriteLine($"> {_congratsOrSympathies} {_passOrFail} with {_properArticle} {_letter}{_letterSymbol}.");
+        {
+            _properArticle = "a";
+        }
+
+
+
+        // Final output
+        Console.WriteLine(
+            $"> {_congratsOrSympathies} [{_passOrFail}] with {_properArticle} [{_letter}{_letterSymbol}]!"
+        );
     }
 }
